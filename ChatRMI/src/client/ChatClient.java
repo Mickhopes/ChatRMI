82b7428f516b3745db83db5e2b2cc657790b2ea7
Mickhopes <mickael.turnel@gmail.com>
2016-02-11 12:38:03 +0100

    Ajout de l'interface graphique du client

---
+++ ChatRMI/src/client/ChatClient.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+/**
+ * Main class to launch the Chat GUI.
+ * 
+ * @author Mickaël Turnel
+ * @author Line Pouvaret
+ */
+public class ChatClient {
+    
+    /**
+     * Create and launch the Chat GUI.
+     * 
+     * @param args Arguments.
+     */
+    public static void main(String[] args) {
+        // Set the look of the windows
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ChatGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ChatGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ChatGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ChatGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        
+        // Creation of the chat UI
+        ChatGUI chat = new ChatGUI();
+        chat.setTitle("Chat RMI");
+        chat.setVisible(true);
+    }
+}
